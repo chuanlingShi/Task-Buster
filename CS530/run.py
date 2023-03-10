b8e8b51cebbaefee793ac23f6a8c4654c2b57b6b
yiyangii <yiyang6004@gmail.com>
2023-03-10 15:30:26 -0500

    add navi bar

--- CS530/run.py
+++ CS530/run.py
@@ -3,9 +3,15 @@ from flask import (Flask, render_template, request)
 app = Flask(__name__)
 
 
-@app.route('/home')
+@app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/home')
+def home1():
+    return render_template('home.html')
+
 
 @app.route('/note')
 def note():
@@ -15,6 +21,12 @@ def note():
 @app.route('/project')
 def project():
     return render_template('project.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080, debug=True)
