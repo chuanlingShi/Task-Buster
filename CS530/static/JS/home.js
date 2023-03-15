a4a5efdc6adb96d2a4dd8a69b9d426651ea90c38
Minyi1113 <56332687+Minyi1113@users.noreply.github.com>
2023-03-15 00:50:26 -0400

    Create home.js

---
+++ CS530/static/JS/home.js
@@ -0,0 +1,17 @@
+function loadTasks() {
+    fetch('/get-Mytasks')
+        .then(response => response.json())
+        .then(tasks => {
+            const taskList = document.getElementById('task-list');
+            taskList.innerHTML = '';
+
+            for (const id in tasks) {
+                const task = tasks[id];
+                const taskElement = document.createElement('div');
+                taskElement.innerHTML = `<h3>${task.title}</h3><p>${task.description}</p>`;
+                taskList.appendChild(taskElement);
+            }
+        });
+}
+
+loadTasks(); // call the function to load the tasks when the page loads
